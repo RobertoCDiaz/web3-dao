ed68aeba7e670baf1c871be263471eca6afa1f10
Roberto Díaz <robertocdiazsanchez@gmail.com>
2022-06-15 00:03:00 -0500

    created a contract for a fake nft marketplace
    The FakeNFTMarketplace exposes some basic functions that we will be using from the DAO contract to purchase NFTs if a proposal is passed. A real NFT marketplace would be more complicated - as not all NFTs have the same price.

---
+++ hardhat/contracts/FakeNFTMarketplace.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract FakeNFTMarketplace {
+    // keeps a map between tokensIds and their owner's address
+    // tokens without owner will have a 0x000... address associated
+    mapping(uint256 => address) tokens;
+
+    // sets the purchase price for an NFT
+    uint256 price = 0.01 ether;
+
+    // purchases the NFT with `tokenId` id.
+    function purchase(uint256 tokenId) external payable {
+        require(msg.value == price, "This token costs 0.01 ether");
+        
+        tokens[tokenId] = msg.sender;
+    }
+
+    // return the price of one NFT
+    function getPrice() external view returns(uint256) {
+        return price;
+    }
+
+    // checks whether a token has been sold or not
+    function available(uint256 tokenId) external view returns(bool) {
+        return tokens[tokenId] == address(0);
+    }
+}
